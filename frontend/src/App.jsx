73d7052200db47443b848c9e63bc8bbd62b8081a
nawapons0 <nawapon.s@cvc.ac.th>
2022-11-01 19:40:42 +0700

    first and complete commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Navbar, Container, Row, Col } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import CreateProduct from './components/CreateProduct';
+import EditProduct from './components/EditProduct/';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <Navbar bg="primary">
+        <Container>
+          <Link to={"/"} className="navbar-brand text-white">
+            React & Laravel CRUD App
+          </Link>
+        </Container>
+      </Navbar>
+
+      <Container className="mt-5">
+        <Row>
+          <Col md={12}>
+            <Routes>
+              <Route exact path="/" element={<ProductList />} />
+              <Route path="/product/create" element={<CreateProduct />} />
+              <Route path="/product/edit/:id" element={<EditProduct />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
+    </Router>
+  )
+}
+
+export default App
